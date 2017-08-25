8688abf562e71ace06f26eaae70bba49fcaf7f26
sternj20 <jessesternmusic@gmail.com>
2017-08-24 20:35:34 -0700

    trying to remove questionid from model

--- models/activity.js
+++ models/activity.js
@@ -1,6 +1,5 @@
 module.exports=function(sequelize, Datatypes){
 	var Activity = sequelize.define("Activity", {
-		question_id: Datatypes.INTEGER,
 		journal_entry: Datatypes.STRING,
 	});
 	Activity.associate = function(models){
